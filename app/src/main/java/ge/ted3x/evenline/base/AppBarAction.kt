b9f737be1a18ccdb02d9e1ef33d20b679d527d74
Tedo Manvelidze <67691418+ted3x@users.noreply.github.com>
2023-06-22 17:38:11 +0400

    # Add AppBarAction.kt
    Signed-off-by: Tedo Manvelidze <67691418+ted3x@users.noreply.github.com>

---
+++ app/src/main/java/ge/ted3x/evenline/base/AppBarAction.kt
@@ -0,0 +1,45 @@
+/*
+ * Created by Tedo Manvelidze(ted3x) on 6/22/23, 5:38 PM
+ * Copyright (c) 2023 . All rights reserved.
+ * Last modified 6/22/23, 5:24 PM
+ */
+
+package ge.ted3x.evenline.base
+
+import android.content.Context
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.ColorRes
+import androidx.annotation.DrawableRes
+import androidx.annotation.StyleRes
+import com.google.android.material.button.MaterialButton
+import ge.ted3x.evenline.R
+
+sealed class AppBarAction {
+
+    abstract fun getView(context: Context): View
+    data class Icon(@DrawableRes val iconRes: Int, val onClick: () -> Unit) : AppBarAction() {
+        override fun getView(context: Context): View {
+            return MaterialButton(context, null, R.style.Button_AppBar_Action).apply {
+                setOnClickListener { onClick.invoke() }
+                setIconResource(iconRes)
+            }
+        }
+    }
+
+    data class Text(
+        val text: String,
+        @StyleRes val textStyle: Int,
+        @ColorRes val textColorRes: Int,
+        val onClick: () -> Unit
+    ) : AppBarAction() {
+        override fun getView(context: Context): View {
+            return TextView(context).apply {
+                this.text = text
+                setTextAppearance(textStyle)
+                setTextColor(context.getColor(textColorRes))
+                setOnClickListener { onClick.invoke() }
+            }
+        }
+    }
+}
