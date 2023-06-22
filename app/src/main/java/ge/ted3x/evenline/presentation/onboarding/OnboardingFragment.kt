8be5407244695687a8e0c7189f03ed25b3d1f270
Tedo Manvelidze <67691418+ted3x@users.noreply.github.com>
2023-06-22 19:38:43 +0400

    # Add navigation to Auth from onboarding
    Signed-off-by: Tedo Manvelidze <67691418+ted3x@users.noreply.github.com>

--- app/src/main/java/ge/ted3x/evenline/presentation/onboarding/OnboardingFragment.kt
+++ app/src/main/java/ge/ted3x/evenline/presentation/onboarding/OnboardingFragment.kt
@@ -1,7 +1,7 @@
 /*
- * Created by Tedo Manvelidze(ted3x) on 6/22/23, 7:35 PM
+ * Created by Tedo Manvelidze(ted3x) on 6/22/23, 7:38 PM
  * Copyright (c) 2023 . All rights reserved.
- * Last modified 6/22/23, 7:35 PM
+ * Last modified 6/22/23, 7:37 PM
  */
 
 package ge.ted3x.evenline.presentation.onboarding
@@ -10,9 +10,13 @@ import dagger.hilt.android.AndroidEntryPoint
 import ge.ted3x.evenline.R
 import ge.ted3x.evenline.base.AppBarAction
 import ge.ted3x.evenline.base.BaseFragment
+import ge.ted3x.evenline.databinding.FragmentOnboardingBinding
+import ge.ted3x.evenline.extensions.viewBinding
 
 @AndroidEntryPoint
 class OnboardingFragment : BaseFragment<OnboardingViewModel>(R.layout.fragment_onboarding) {
+
+    private val binding by viewBinding(FragmentOnboardingBinding::bind)
 
     override val viewModelClass = OnboardingViewModel::class
 
@@ -29,5 +33,6 @@ class OnboardingFragment : BaseFragment<OnboardingViewModel>(R.layout.fragment_o
 
     override fun onBindViewModel(vm: OnboardingViewModel) {
         vm.setIsOnboarded()
+        binding.onboardingGetStartedBtn.setOnClickListener { vm.navigateToAuth() }
     }
 }
--- app/src/main/java/ge/ted3x/evenline/presentation/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/ge/ted3x/evenline/presentation/onboarding/OnboardingViewModel.kt
@@ -1,24 +1,31 @@
 /*
- * Created by Tedo Manvelidze(ted3x) on 6/22/23, 5:39 PM
+ * Created by Tedo Manvelidze(ted3x) on 6/22/23, 7:38 PM
  * Copyright (c) 2023 . All rights reserved.
- * Last modified 6/22/23, 5:39 PM
+ * Last modified 6/22/23, 7:37 PM
  */
 
 package ge.ted3x.evenline.presentation.onboarding
 
+import com.github.terrakok.cicerone.Router
 import dagger.hilt.android.lifecycle.HiltViewModel
 import ge.ted3x.evenline.base.BaseViewModel
 import ge.ted3x.evenline.data.SharedPreferencesHandler
+import ge.ted3x.evenline.screens.Screens
 import ge.ted3x.evenline.utils.EvenlineConstants
 import javax.inject.Inject
 
 @HiltViewModel
 class OnboardingViewModel @Inject constructor(
-    private val sharedPreferencesHandler: SharedPreferencesHandler
+    private val sharedPreferencesHandler: SharedPreferencesHandler,
+    private val router: Router
 ) :
     BaseViewModel() {
 
     fun setIsOnboarded() {
         sharedPreferencesHandler.writeBoolean(EvenlineConstants.EVENLINE_IS_ONBOARDED, true)
+    }
+
+    fun navigateToAuth() {
+        router.newRootScreen(Screens.Auth())
     }
 }
